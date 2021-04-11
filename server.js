44051b64a1e140a29394272eb8957aa970b4b920
Saul Soto <saulivan852@gmail.com>
2021-04-10 22:32:57 -0700

    Added express server file

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+
+const app = express();
+
+//serves files from our dist directory wich now contains our index.html file
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log("Arkonnect running on: " + port);
