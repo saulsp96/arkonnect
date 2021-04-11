57a11a5f208fc8a23338ede1af3092bc75167823
MarcoAnRamirez <61001054+MarcoAnRamirez@users.noreply.github.com>
2021-04-11 14:20:16 -0700

    Update TopBanner.vue

--- src/components/TopBanner.vue
+++ src/components/TopBanner.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card class="grey lighten-4" max-height="70">
     <v-toolbar flat class="grey lighten-4">
-      <v-toolbar-title>Dashboard</v-toolbar-title>
+      <v-toolbar-title>Bienvenido a Arkonnect</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
 
