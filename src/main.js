09e9b70fdb209c3d13beab8bb3a05f4bb72733d7
MarcoAnRamirez <61001054+MarcoAnRamirez@users.noreply.github.com>
2021-04-11 14:18:27 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+Vue.component('loading-overlay', Loading)
 
 Vue.config.productionTip = false;
 
