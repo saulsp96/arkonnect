28c0e40d58acff21b760d2b68950b85c525bf1e3
Saul Soto <saulivan852@gmail.com>
2021-05-13 17:59:36 -0700

    Added all other paths

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express();
 //serves files from our dist directory wich now contains our index.html file
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 app.use("/dashboard01", serveStatic(path.join(__dirname, "/dist")));
-
+app.use("/dashboard02", serveStatic(path.join(__dirname, "/dist")));
+app.use("/dashboard03", serveStatic(path.join(__dirname, "/dist")));
+app.use("/dashboard04", serveStatic(path.join(__dirname, "/dist")));
 const port = process.env.PORT || 8080;
 app.listen(port);
 
