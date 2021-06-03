06dc2361cb5d612f4469de025c3cbcc0aa31a80c
Saul Soto <saulivan852@gmail.com>
2021-06-02 19:03:19 -0700

    Added getColor to Ingles field on tables

--- src/components/table01.vue
+++ src/components/table01.vue
@@ -7,12 +7,30 @@
       :items-per-page="Talento.length"
       class="elevation-1"
     >
-    </v-data-table
-  ></v-container>
+      <template v-slot:[`item.Activo\nMT`]="{ item }">
+        <v-simple-checkbox
+          v-model="item['Activo MT']"
+          disabled
+        ></v-simple-checkbox>
+      </template>
+      <template v-slot:[`item.Ingles`]="{ item }">
+        <v-chip :color="getColor(item.Ingles)" dark>
+          {{ item.Ingles }}
+        </v-chip>
+      </template>
+    </v-data-table></v-container
+  >
 </template>
 
 <script>
 export default {
+  methods: {
+    getColor(Ingles) {
+      if (Ingles < 5) return "red";
+      else if (Ingles < 8) return "orange";
+      else return "green";
+    },
+  },
   data() {
     return {
       Headers: this.$store.state.headersAdmin,
--- src/components/table02.vue
+++ src/components/table02.vue
@@ -7,16 +7,22 @@
       :items-per-page="Talento.length"
       class="elevation-1"
     >
-    </v-data-table
-  ></v-container>
+      <template v-slot:[`item.Ingles`]="{ item }">
+        <v-chip :color="getColor(item.Ingles)" dark>
+          {{ item.Ingles }}
+        </v-chip>
+      </template>
+    </v-data-table></v-container
+  >
 </template>
 
 <script>
 export default {
   methods: {
-    getColor(calories) {
-      if (calories > 400) return "red";
-      else if (calories > 200) return "orange";
+    getColor(Ingles) {
+      console.log("This is Ingles" + Ingles);
+      if (Ingles < 5) return "red";
+      else if (Ingles < 8) return "orange";
       else return "green";
     },
   },
